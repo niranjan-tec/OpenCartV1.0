731dbf78a4274b3ef0f8b47eee4fd761e731b6d0
Niranjan <niranjankumar2252@gmail.com>
2025-04-09 17:31:57 +0530

    All files

---
+++ src/test/java/pageObjects/RemoveItems.java
@@ -0,0 +1,51 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+public class RemoveItems extends BasePage{
+	
+	public RemoveItems(WebDriver driver) {
+
+		super(driver);
+
+	}
+
+	@FindBy(xpath = "//a[normalize-space()='Desktops']")
+	WebElement deskOption;
+
+	@FindBy(xpath = "//a[normalize-space()='Mac (1)']")
+	WebElement selectOptMac;
+
+	@FindBy(xpath = "//span[normalize-space()='Add to Cart']")
+	WebElement btnAddToCart;
+
+	@FindBy(xpath = "//span[@id='cart-total']")
+	WebElement btnCartTotal;
+
+	@FindBy(xpath = "//button[@title='Remove']")
+	WebElement remProduct;
+	
+	@FindBy(xpath = "//p[text()='Your shopping cart is empty!']")
+	WebElement empText;
+
+	public void VerifyAddAndRemoveProductFuctionality() throws InterruptedException {
+		deskOption.click();
+		selectOptMac.click();
+		btnAddToCart.click();
+		Thread.sleep(2000);
+		btnCartTotal.click();
+		Thread.sleep(2000);
+		remProduct.click();
+		Thread.sleep(2000);
+		btnCartTotal.click();
+		String emptTest=empText.getText();
+		System.out.println(emptTest);
+		Assert.assertEquals(emptTest, "Your shopping cart is empty!");
+
+	}
+
+
+}
--- src/test/java/testCases/TC_005_RemoveItemsTest.java
+++ src/test/java/testCases/TC_005_RemoveItemsTest.java
@@ -1,20 +1,19 @@
 package testCases;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import pageObjects.AddToCart;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
+import pageObjects.RemoveItems;
 import testBase.BaseClass;
 
-public class TC_004_AddToCartTest extends BaseClass{
-	
-	
-	
-	@Test(priority =4 )
+public class TC_005_RemoveItemsTest extends BaseClass{
+
+	@Test(priority =5 )
 	public void login() {
 
-		logger.info("** Start Add to Cart Test TC_004 **");
+		logger.info("** Start Remove Items Test TC_005 **");
 
 		try {
 			
@@ -24,22 +23,24 @@ public class TC_004_AddToCartTest extends BaseClass{
 			hp.clickLogin();
 			
 			// Login Page
-			logger.info("** Enter the login details**");
+			logger.info("** Enter the login details **");
 			LoginPage lp = new LoginPage(driver);
 			lp.setEmail(p.getProperty("email"));			
 			lp.setPassword(p.getProperty("password"));
 			lp.clickLogin();
 			
 		
-			AddToCart atc= new AddToCart(driver);
-			logger.info("** Add to cart **");
-			atc.addMacProductInCart();
+			RemoveItems remProdut= new RemoveItems(driver);
+			logger.info("** Add item into cart and removed it **");
+			remProdut.VerifyAddAndRemoveProductFuctionality();
 			
 		} catch (Exception e) {
+			
 			Assert.fail();
+			
 		}
 
-		logger.info("** Finished Add To Cart Test TC_004 **");
+		logger.info("** Finished The Remove Items Test TC_005 **");
 	}
 
 }
